c280af3e00c7cbb1405b997f6fd3d0d155c708b9
select766 <select766@outlook.jp>
2023-03-10 21:13:20 +0900

    ランダムプレイヤー

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <random>
 
 using namespace std;
 
@@ -12,6 +13,9 @@ using namespace std;
 
 int main()
 {
+    std::random_device seed_gen;
+    mt19937 engine(seed_gen());
+
     int id; // id of your player.
     cin >> id; cin.ignore();
     int board_size;
@@ -25,14 +29,20 @@ int main()
         }
         int action_count; // number of legal actions for this turn.
         cin >> action_count; cin.ignore();
+
+        vector<string> actions;
         for (int i = 0; i < action_count; i++) {
             string action; // the action
             cin >> action; cin.ignore();
+            actions.push_back(action);
         }
 
         // Write an action using cout. DON'T FORGET THE "<< endl"
         // To debug: cerr << "Debug messages..." << endl;
+        uniform_int_distribution<> dist(0, actions.size() - 1);
 
-        cout << "f4" << endl; // a-h1-8
+        int action_idx = dist(engine);
+
+        cout << actions[action_idx] << endl; // a-h1-8
     }
 }
