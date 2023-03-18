ddcfe2d8ffb9f41d264593d489a1b328c638e3a3
select766 <select766@outlook.jp>
2023-03-18 10:25:39 +0900

    合法手のテストルーチン

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # codingame-othello
 CodingameのOthello(オセロ)タスクのAI
+
+# 合法手チェック
+
+## データ生成
+
+正しく動作するコミットにおいて、`MODE_MAKE_LEGAL_MOVE_TEST_DATA`マクロを定義した状態でビルド
+
+```
+./src/main > dataset/legal_move_dataset.txt
+```
+
+## チェック
+
+検証したいコミットにおいて、`MODE_LEGAL_MOVE_TEST`マクロを定義した状態でビルド
+
+```
+./src/main < dataset/legal_move_dataset.txt
+```
--- src/main.cpp
+++ src/main.cpp
@@ -140,7 +140,7 @@ public:
     // 盤面を表現する、- (なし), O (白), X (黒)を64文字並べた文字列を返す
     string get_position_string()
     {
-        char str[BOARD_AREA+1];
+        char str[BOARD_AREA + 1];
         str[BOARD_AREA] = '\0';
         int i = 0;
         for (int row = 0; row < BOARD_SIZE; row++)
@@ -181,7 +181,7 @@ public:
     void set_position_string(const string &position, int turn)
     {
         // 注意: パス情報については保存できない
-        const char* pchars = position.c_str();
+        const char *pchars = position.c_str();
         memset(board, 0, N_PLAYER * BOARD_AREA * sizeof(int));
         total_stones = 0;
         int i = 0;
@@ -212,7 +212,7 @@ public:
     // get_position_string_with_turn()の結果を読み取る
     void set_position_string_with_turn(const string &position_with_turn)
     {
-        char turn_char = position_with_turn[BOARD_AREA + 2];
+        char turn_char = position_with_turn[BOARD_AREA + 1];
         set_position_string(position_with_turn, turn_char == 'b' ? BLACK : WHITE); // 余分な文字がついていても先頭64文字しか見ない
     }
 
@@ -246,7 +246,8 @@ public:
         last_pass[1] = undo_info.last_pass[1];
     }
 
-    void legal_moves(vector<int> &move_list) const
+    // 合法手を列挙する。
+    void legal_moves(vector<int> &move_list, bool with_pass = false) const
     {
         move_list.clear();
         for (int pos = 0; pos < BOARD_AREA; pos++)
@@ -255,6 +256,11 @@ public:
             {
                 move_list.push_back(pos);
             }
+        }
+
+        if (with_pass && move_list.empty())
+        {
+            move_list.push_back(MOVE_PASS);
         }
     }
 
@@ -553,7 +559,8 @@ public:
             // 黒から見たスコアなので、手番から見たスコアにする
             // 乱数要素がないと強さ測定が難しいので入れている
             int score = board.count_stone_diff() * 256 + dist(engine);
-            if (board.turn() != BLACK) {
+            if (board.turn() != BLACK)
+            {
                 score = -score;
             }
             return score;
@@ -565,18 +572,22 @@ public:
         {
             move_list.push_back(MOVE_PASS);
         }
-        for (auto move : move_list) {
+        for (auto move : move_list)
+        {
             UndoInfo undo_info;
             board.do_move(move, undo_info);
             int child_score = -alphabeta(depth - 1, -beta, -alpha, nullptr);
             board.undo_move(undo_info);
-            if (child_score > alpha) {
-                if (bestmove != nullptr) {
+            if (child_score > alpha)
+            {
+                if (bestmove != nullptr)
+                {
                     *bestmove = move;
                 }
                 alpha = child_score;
             }
-            if (alpha >= beta) {
+            if (alpha >= beta)
+            {
                 return alpha;
             }
         }
@@ -762,6 +773,109 @@ int main()
     }
 
     return 0;
+}
+#elif defined(MODE_LEGAL_MOVE_TEST)
+// 合法手生成が正しいかテストする
+vector<string> string_split(const string &str, char sep)
+{
+    vector<string> elems;
+    string::size_type len = str.length();
+
+    for (string::size_type i = 0, n; i < len; i = n + 1)
+    {
+        n = str.find_first_of(sep, i);
+        if (n == string::npos)
+        {
+            n = len;
+        }
+        elems.push_back(str.substr(i, n - i));
+    }
+
+    return elems;
+}
+
+int main()
+{
+    string test_case;
+    int i = 0;
+    int ok = 0;
+    while (getline(cin, test_case))
+    {
+        i++;
+        // 入力: 局面/合法手カンマ区切り/合法手1で進めた局面/合法手2で進めた局面...
+        auto elems = string_split(test_case, '/');
+
+        Board board;
+        board.set_position_string_with_turn(elems[0]);
+        auto legal_moves_str = string_split(elems[1], ',');
+        vector<int> expected_legal_moves;
+        for (auto legal_move_str : legal_moves_str)
+        {
+            expected_legal_moves.push_back(move_from_str(legal_move_str));
+        }
+        vector<int> sorted_expected_legal_moves = expected_legal_moves;
+        sort(sorted_expected_legal_moves.begin(), sorted_expected_legal_moves.end());
+
+        vector<int> actual_legal_moves;
+        board.legal_moves(actual_legal_moves, true);
+        sort(actual_legal_moves.begin(), actual_legal_moves.end());
+
+        if (sorted_expected_legal_moves != actual_legal_moves)
+        {
+            cout << "In case " << i << endl;
+            cout << test_case << endl;
+            cout << "Expected legal moves:";
+            for (auto move : expected_legal_moves)
+            {
+                cout << " " << move;
+            }
+            cout << endl;
+            cout << "Actual legal moves:";
+            for (auto move : actual_legal_moves)
+            {
+                cout << " " << move;
+            }
+            cout << endl;
+
+            continue;
+        }
+
+        if (actual_legal_moves[0] != MOVE_PASS)
+        {
+            // 各合法手で進めてみる
+            int elem_idx = 2;
+            bool success = true;
+            for (auto move : expected_legal_moves)
+            {
+                UndoInfo undo_info;
+                board.do_move(move, undo_info);
+                auto actual_board = board.get_position_string_with_turn();
+                if (actual_board != elems[elem_idx])
+                {
+                    cout << "In case " << i << endl;
+                    cout << test_case << endl;
+                    cout << "Board after move " << move_to_str(move) << " does not match." << endl;
+                    cout << elems[elem_idx] << " != " << actual_board << endl;
+                    success = false;
+                    break;
+                }
+                board.undo_move(undo_info);
+
+                elem_idx++;
+            }
+
+            if (!success)
+            {
+                continue;
+            }
+        }
+
+        ok++;
+    }
+
+    cout << i << " cases, " << ok << " passed, " << (i - ok) << " failed" << endl;
+
+    return i == ok ? 0 : 1;
 }
 #else
 int main()
